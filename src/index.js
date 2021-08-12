439fae41eb4e8d4cb2e3852572f345827059a432
ola <o.bukhal@gmail.com>
2021-08-12 14:11:11 +0300

    feat: add morse decode function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    function transformToMorse (str) {
+        // debugger;
+        let morseStr = '';
+        for (let i = 0; i <= str.length-2; i = i+2) {
+            let code = str[i]+str[i+1];
+                if( code == '10') {
+                  morseStr += '.';
+                } else if( code == '11') {
+                  morseStr += '-';
+                }
+            }
+        return morseStr;    
+    } 
+
+
+    // seporate string to words inside string
+    let str = expr.split('*'.repeat(10)); 
+    let words = [];
+    // debugger;
+    for (let el of str) {
+        let word = '';
+        let letter = '';
+        for (let i = 0; i < el.length; i++) {
+            let symbol = el[i];
+            letter += symbol;
+            if (letter.length == 10) {
+                letter = transformToMorse(letter);
+                letter = MORSE_TABLE[letter];
+                word += letter;
+                letter = '';
+                continue;
+            }
+        }
+        words.push(word);
+    }
+ 
+    return words.join(' ');
 }
+
+
+
 
 module.exports = {
     decode
 }
+
+
